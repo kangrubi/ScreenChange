0d21f39dec2933324b24d244d1350b8aca1335d6
rubi kang <rubi0624@nate.com>
2022-01-29 19:09:19 +0900

    Initial Commit

---
+++ ScreenChange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenChange
+//
+//  Created by 강루비 on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
